996c89e48dca0bc00a3bc80c23526711beba7f0f
celinewu <cywu@caltech.edu>
2025-06-23 13:36:51 -0400

    cleaning up

--- CustomAgent_Smolagent/setup.py
+++
@@ -1,8 +0,0 @@
-# setup.py
-from setuptools import setup, find_packages
-
-setup(
-    name='crdc_agent',
-    version='0.1',
-    packages=find_packages(),  # Automatically finds 'tools' if it has __init__.py
-)
